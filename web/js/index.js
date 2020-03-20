349eb8434429aa8dd4064b562d62c62c22df3524
Lan <lan.vukusic@gmail.com>
2020-03-20 16:08:33 +0100

    repaired overflow issues with CLI on chrome

--- web/js/index.js
+++ web/js/index.js
@@ -13,7 +13,7 @@ function clearDevices() {
 function cliLog(level, type, msg) {
   let time = new Date()
   let timeFormated = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "." + time.getMilliseconds().toString()
-  let cli = $(".cli")
+  let cli = $("#cliLog")
   let message = $('<div class="cli_line"><div class="cli_time_stamp">' + timeFormated + '</div><div class="cli_type">' + type + '</div><div class="cli_body">' + msg + '</div></div>');
 
   if (level == 0) {
